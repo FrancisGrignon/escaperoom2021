6802878c1ef936dc0f86857c49673f7355e3fe28
Francis Grignon <live.com@ncis.ca>
2021-05-17 22:09:56 -0400

    Add a file upload form

---
+++ src/Web/WebAdmin/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebAdmin.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+
+                using (var stream = new MemoryStream())
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            ViewBag["Message"] = "File successfully uploaded.";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/Web/WebAdmin/Models/FileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAdmin.Models
+{
+    public abstract class FileModel
+    {
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public string Extension { get; set; }
+    }
+}
